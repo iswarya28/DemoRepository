8d2c7193577a1e01d820aced4eb53d64acdc3002
Sivaramulu <inisivaram@ad.harman.com>
2018-12-17 15:33:24 +0530

    pushing the action class code

---
+++ src/com/action/keyboard/test/ActionTest.java
@@ -0,0 +1,46 @@
+package com.action.keyboard.test;
+
+import java.awt.AWTException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class ActionTest {
+
+	public static void main(String args[]) throws AWTException {
+		System.setProperty("webdriver.gecko.driver", "D:\\Browse\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://toolsqa.com/automation-practice-form/");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		  
+		WebDriverWait wait = new WebDriverWait(driver, 6);
+		
+		wait.until(ExpectedConditions.titleContains("Demo Form"));
+		  
+		WebElement tutorialElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(), 'Tutorial')]")));
+		    
+		Actions action = new Actions(driver);
+		action.moveToElement(tutorialElement).build().perform();
+		try {
+		Thread.sleep(2000);
+		} catch (InterruptedException e) {
+		e.printStackTrace();
+		}
+		action.moveToElement(driver.findElement(By.xpath("//span[contains(text(), 'Web Automation Tools')]"))).build().perform();
+		try {
+		Thread.sleep(2000);
+		} catch (InterruptedException e) {
+		e.printStackTrace();
+		}
+		action.moveToElement(driver.findElement(By.xpath("//span[contains(text(), 'Cucumber')]"))).click().build().perform();
+		  
+		 // driver.close();  
+		 }
+		}
